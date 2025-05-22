2336f5caaad875c311e7fb3128790384ca7cdfbc
Juvvanapudi Vijay <juvvanapudivijay123@gmail.com>
2025-05-23 00:43:17 +0530

    Add files via upload

---
+++ assets/js/bannerslider.js
@@ -0,0 +1,10 @@
+$('.banner-slider').slick({
+    autoplay: true, // This enables automatic sliding
+    autoplaySpeed: 3000, // Time between slides (in milliseconds)
+    // Other options...
+});
+$('.banner-slider').slick({
+    autoplay: false, // Disable automatic sliding
+    // Other options...
+});
+$('.banner-slider').slick('slickPause');
